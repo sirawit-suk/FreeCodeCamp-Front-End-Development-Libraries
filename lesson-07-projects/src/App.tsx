49969187159a76a9ae8e6a330823d4df566cda49
Sirawit <popeye-zz@hotmail.com>
2023-01-15 17:27:39 +0700

    add react router 6

--- lesson-07-projects/src/App.tsx
+++ lesson-07-projects/src/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <div> Hello world! </div>;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+// export for unit testing in the future
+// doc: https://www.youtube.com/watch?v=cchqeWY0Nak&t=1581s
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App;
+export function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
---
+++ lesson-07-projects/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <>
+      <div>NotFound</div>
+      <Link to="/">Go home!</Link>
+    </>
+  );
+}
+
+export default NotFound;
