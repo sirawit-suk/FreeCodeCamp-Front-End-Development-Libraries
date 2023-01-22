2c8fae732ea25cb8c245785abf18f647167be99a
Sirawit <popeye-zz@hotmail.com>
2023-01-22 20:42:40 +0700

    style: fix icon nav and drum in mobile

--- lesson-07-projects/src/shared/NavBar.tsx
+++ lesson-07-projects/src/shared/NavBar.tsx
@@ -23,7 +23,7 @@ export function NavBar() {
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="scale-150 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="ml-2 scale-150 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   {open ? (
                     <FontAwesomeIcon icon={faCaretUp} />
                   ) : (
